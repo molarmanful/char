622cc72fb7fa6de3acdd82d35816996ef0fab8aa
Ben Pang <benjinpang@gmail.com>
2016-07-29 08:17:13 -0700

    initial usable game

---
+++ game.js
@@ -0,0 +1,115 @@
+//init
+floor=` `
+wall=`▓`
+player=`@`
+enemy=`&`
+field=`#`
+pc=0
+pr=0
+mc=120
+mr=50
+ec=119
+er=49
+map=Array(mr).fill(Array(mc).fill(floor))
+score=0
+die=0
+Die=0
+mR=x=>((x%mr)+mr)%mr
+mC=x=>((x%mc)+mc)%mc
+
+//update map function
+um=_=>{
+  x.innerHTML=map.map(a=>a.join``).join`\n`.replace(player,`<span style='color:limegreen'>${player}</span>`).replace(enemy,`<span style='color:red'>${enemy}</span>`)+`\n\nScore: `+score/10
+}
+//update coords functions
+up=(R,C,p)=>map=map.map((a,b)=>a.map((c,d)=>b^R||d^C?c==p?floor:c:p))
+Up=(R,C,p)=>map=map.map((a,b)=>a.map((c,d)=>b^R||d^C?c:p))
+
+//init walls
+map=map.map((a,b)=>a.map((c,d)=>Math.random()*2|0&&Math.random()*2|0?wall:c))
+
+//init map
+up(pr,pc,player)
+up(er,ec,enemy)
+um()
+
+//key states
+k={}
+onkeydown=e=>k[e.which]=1
+onkeyup=e=>k[e.which]=0
+
+//movements
+pm=setInterval(_=>{
+  //make sure keydown
+  if(Object.getOwnPropertyNames(k).length){
+    //player
+    if(Die)
+      end();
+    else{
+      //w
+      if(k[87]&&(map[mR(pr-1)][pc]==floor||map[mR(pr-1)][pc]==field))
+        pr=mR(pr-1);
+      //s
+      if(k[83]&&(map[mR(pr+1)][pc]==floor||map[mR(pr+1)][pc]==field))
+        pr=mR(pr+1);
+      //a
+      if(k[65]&&(map[pr][mC(pc-1)]==floor||map[pr][mC(pc-1)]==field))
+        pc=mC(pc-1);
+      //d
+      if(k[68]&&(map[pr][mC(pc+1)]==floor||map[pr][mC(pc-1)]==field))
+        pc=mC(pc+1);
+      //forcefield
+      if(k[32])
+        [...Array(7).keys()].map((a,b)=>(b=pr+b-3,[...Array(7).keys()].map((c,d)=>(d=pc+d-3,Up(b,d,field))))),
+        score-=50;
+    }
+
+    //enemy
+    if(die)
+      er=49,ec=119,die=0;
+    else{
+      r=Math.random()*100|0
+      //up
+      if(r>=0&&r<10)
+        er--;
+      //down
+      if(r>=10&&r<20)
+        er++;
+      //left
+      if(r>=20&&r<30)
+        ec--;
+      //right
+      if(r>=30&&r<40)
+        ec++;
+      //teleport
+      if(r==50)
+        er=pr+Math.random()*5|0+Math.random()*-2|0,
+        ec=pc+Math.random()*5|0*-2|0,
+        map=map.map((a,b)=>a.map((c,d)=>c==floor||c==wall||c==field?Math.random()*2|0&&Math.random()*2|0?wall:floor:c));
+    }
+    //enemy field death
+    map[er]&&map[er][ec]==field&&(die=1)
+    //update
+    up(mR(pr),mC(pc),player)
+    up(mR(er),mC(ec),enemy)
+    um()
+  }
+},50)
+
+//scorekeeping
+sc=setInterval(_=>{score++,um()},100)
+
+//check for deaths
+d=setInterval(_=>{
+  (x.innerHTML.match(player)&&(er^pr||ec^pc)&&score>=0)||(Die=1)
+},1)
+
+//game over
+end=_=>{
+  clearInterval(sc)
+  clearInterval(pm)
+  clearInterval(d)
+  setTimeout(_=>{
+    x.innerHTML=`You died!\n Survived for ${score/10} seconds.`
+  },2000)
+}
