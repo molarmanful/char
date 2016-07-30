9859afb0fdbfbec3e879e3add89726b20ae92ea7
Ben Pang <benjinpang@gmail.com>
2016-07-29 18:23:40 -0700

    add start page, more updates

--- game.js
+++ game.js
@@ -3,7 +3,8 @@ floor=` `
 wall=`▓`
 player=`@`
 enemy=`&`
-field=`#`
+field=`+`
+mine=`ø`
 pc=0
 pr=0
 mc=120
@@ -11,7 +12,7 @@ mr=50
 ec=119
 er=49
 map=Array(mr).fill(Array(mc).fill(floor))
-score=0
+score=1
 die=0
 Die=0
 mR=x=>((x%mr)+mr)%mr
@@ -47,53 +48,60 @@ pm=setInterval(_=>{
       end();
     else{
       //w
-      if(k[87]&&(map[mR(pr-1)][pc]==floor||map[mR(pr-1)][pc]==field))
+      if(k[87]&&map[mR(pr-1)][pc]!=wall)
         pr=mR(pr-1);
       //s
-      if(k[83]&&(map[mR(pr+1)][pc]==floor||map[mR(pr+1)][pc]==field))
+      if(k[83]&&map[mR(pr+1)][pc]!=wall)
         pr=mR(pr+1);
       //a
-      if(k[65]&&(map[pr][mC(pc-1)]==floor||map[pr][mC(pc-1)]==field))
+      if(k[65]&&map[pr][mC(pc-1)]!=wall)
         pc=mC(pc-1);
       //d
-      if(k[68]&&(map[pr][mC(pc+1)]==floor||map[pr][mC(pc-1)]==field))
+      if(k[68]&&map[pr][mC(pc+1)]!=wall)
         pc=mC(pc+1);
       //forcefield
       if(k[32])
-        [...Array(7).keys()].map((a,b)=>(b=pr+b-3,[...Array(7).keys()].map((c,d)=>(d=pc+d-3,Up(b,d,field))))),
-        score-=50;
+        score/=2,score|=0,
+        [...Array(7).keys()].map((a,b)=>(b=mR(pr+b-3),[...Array(7).keys()].map((c,d)=>(d=mC(pc+d-3),Up(b,d,field)))));
     }
-
-    //enemy
-    if(die)
-      er=49,ec=119,die=0;
-    else{
-      r=Math.random()*100|0
-      //up
-      if(r>=0&&r<10)
-        er--;
-      //down
-      if(r>=10&&r<20)
-        er++;
-      //left
-      if(r>=20&&r<30)
-        ec--;
-      //right
-      if(r>=30&&r<40)
-        ec++;
-      //teleport
-      if(r==50)
-        er=pr+Math.random()*5|0+Math.random()*-2|0,
-        ec=pc+Math.random()*5|0*-2|0,
-        map=map.map((a,b)=>a.map((c,d)=>c==floor||c==wall||c==field?Math.random()*2|0&&Math.random()*2|0?wall:floor:c));
-    }
-    //enemy field death
-    map[er]&&map[er][ec]==field&&(die=1)
-    //update
-    up(mR(pr),mC(pc),player)
-    up(mR(er),mC(ec),enemy)
-    um()
   }
+
+  //enemy
+  if(die)
+    er=49,ec=119,die=0,score+=500;
+  else{
+    r=Math.random()*100|0
+    //up
+    if(r>=0&&r<10)
+      er--;
+    //down
+    if(r>=10&&r<20)
+      er++;
+    //left
+    if(r>=20&&r<30)
+      ec--;
+    //right
+    if(r>=30&&r<40)
+      ec++;
+    //teleport
+    if(r==50)
+      er=pr+(Math.random()*3+1)|0*(Math.random()*-2|0),
+      ec=pc+(Math.random()*3+1)|0*(Math.random()-2|0),
+      map=map.map((a,b)=>a.map((c,d)=>c==floor||c==wall||c==field||c==mine?Math.random()*2|0&&Math.random()*2|0?wall:floor:c));
+    //mines!
+    if(r==51)
+      [...Array(11).keys()].map((a,b)=>(b=mR(er+b-5),[...Array(11).keys()].map((c,d)=>(d=mC(ec+d-5),Up(b,d,mine)))));
+  }
+
+  //enemy field death
+  map[er]&&map[er][ec]==field&&(die=1)
+  //player mine death
+  map[pr]&&map[pr][pc]==mine&&(score-=100)
+
+  //update
+  up(mR(pr),mC(pc),player)
+  up(mR(er),mC(ec),enemy)
+  um()
 },50)
 
 //scorekeeping
@@ -101,7 +109,7 @@ sc=setInterval(_=>{score++,um()},100)
 
 //check for deaths
 d=setInterval(_=>{
-  (x.innerHTML.match(player)&&(er^pr||ec^pc)&&score>=0)||(Die=1)
+  (x.innerHTML.match(player)&&(er^pr||ec^pc)&&score>0)||(Die=1)
 },1)
 
 //game over
@@ -110,6 +118,6 @@ end=_=>{
   clearInterval(pm)
   clearInterval(d)
   setTimeout(_=>{
-    x.innerHTML=`You died!\n Survived for ${score/10} seconds.`
+    x.innerHTML=`You died!\n Survived for ${score/10} seconds.\nReload the page to play again.`
   },2000)
 }
